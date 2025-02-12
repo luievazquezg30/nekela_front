6a291308ea4e42c5502d72bcb916ba2234621af5
ArtN0nymous <jr616934@gmail.com>
2025-02-12 17:53:56 -0600

    home slide footer in slide up gesture

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnInit, Renderer2, viewChild, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonFooter, IonCard, IonImg } from '@ionic/angular/standalone';
+import { IonContent, IonFooter, IonCard, IonImg, IonItemDivider, GestureController, GestureDetail } from '@ionic/angular/standalone';
 import { TaskCarouselComponent } from "../../components/task-carousel/task-carousel.component";
 import { CapabilitiesCarouselComponent } from "../../components/capabilities-carousel/capabilities-carousel.component";
 @Component({
@@ -9,13 +9,50 @@ import { CapabilitiesCarouselComponent } from "../../components/capabilities-car
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonImg, IonCard, IonContent, CommonModule, FormsModule, IonFooter, TaskCarouselComponent, CapabilitiesCarouselComponent]
+  imports: [IonImg, IonCard, IonContent, CommonModule, FormsModule, IonFooter, TaskCarouselComponent, CapabilitiesCarouselComponent, IonItemDivider]
 })
 export class HomePage implements OnInit {
-
-  constructor() { }
+  @ViewChild(IonContent) content!: ElementRef<HTMLIonContentElement>
+  @ViewChild('footer', {read: ElementRef}) footer!: ElementRef;
+  constructor(private renderer: Renderer2, private gestureCtrl: GestureController, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter', this.footer.nativeElement);
+    if(this.footer.nativeElement){
+      const gesture = this.gestureCtrl.create({
+        el: this.footer.nativeElement, // Detecta el gesto en toda la pantalla
+        gestureName: 'swipe-footer',
+        threshold: 0,
+        onStart: () => {
+          this.cdRef.detectChanges();
+        },
+        onMove: (event) => this.handleGesture(event),
+        onEnd: () => this.onEnd()
+      });
+  
+      gesture.enable();
+    }else{
+      console.log('No se ha encontrado el footer');
+    }
+  }
+
+  private onEnd() {
+    this.cdRef.detectChanges();
+  }
+
+
+  handleGesture(event: GestureDetail) {
+    console.log(event);
+    if (event.deltaY < 30) {
+      this.renderer.addClass(this.footer.nativeElement, 'footer-grow');
+      this.renderer.removeClass(this.footer.nativeElement, 'footer-shrink');
+    } else if (event.deltaY > -30) {
+      this.renderer.addClass(this.footer.nativeElement, 'footer-shrink');
+      this.renderer.removeClass(this.footer.nativeElement, 'footer-grow');
+    }
   }
 
 }
