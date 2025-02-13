8170288db1a97f48015142780aba6c5657a98a30
ArtN0nymous <jr616934@gmail.com>
2025-02-13 11:20:04 -0600

    add confirm modal

--- src/app/components/task-carousel/task-carousel.component.ts
+++ src/app/components/task-carousel/task-carousel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IonIcon } from '@ionic/angular/standalone';
+import { ModalController} from '@ionic/angular/standalone';
+import { ConfirmComponent } from '../confirm/confirm.component';
 
 @Component({
   selector: 'app-task-carousel',
@@ -9,8 +11,26 @@ import { IonIcon } from '@ionic/angular/standalone';
 })
 export class TaskCarouselComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
+
+  openModal(){
+    this.modalCtrl.create({
+      component: ConfirmComponent,
+      componentProps: {
+        title: '¿Quieres inciar tu jornada?',
+      }
+    }).then((modal) => {
+      modal.present();
+      modal.onDidDismiss().then((data) => {
+        if(data.data.confirm){
+          console.log('Jornada iniciada');
+        }else{
+          console.log('Jornada cancelada');
+        }
+      });
+    });
+  }
 
 }
